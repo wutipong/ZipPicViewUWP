cfb5cf9a36664600e57724fdc1dd5f28a71fde84
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2018-07-19 13:04:03 +0700

    Update the thumbnail load text.

--- ZipPicViewUWP/MainPage.xaml.cs
+++ ZipPicViewUWP/MainPage.xaml.cs
@@ -320,7 +320,7 @@ namespace ZipPicViewUWP
 
                 for (int i = 0; i < fileList.Length; i++)
                 {
-                    thumbProgressText.Text = string.Format("Loading {0}/{1}", i, fileList.Length);
+                    thumbProgressText.Text = string.Format("Loading Thumbnails {0}/{1}", i, fileList.Length);
                     thumbProgress.Value = i;
                     await SetThumbnailImage(provider, fileList[i], thumbnails[i], token);
                 }
